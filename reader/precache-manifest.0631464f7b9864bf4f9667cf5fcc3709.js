be719cc32f19288557f6430fe8853542d5030c8a
yongshuang <yongshuang@gdgyun.com>
2019-01-19 10:08:18 +0800

    delete empty floder add reader

---
+++ reader/precache-manifest.0631464f7b9864bf4f9667cf5fcc3709.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "709cf4b9ab69d8bd6f76",
+    "url": "/static/js/main.709cf4b9.chunk.js"
+  },
+  {
+    "revision": "8c0ae54171fdaf6ae6be",
+    "url": "/static/js/1.8c0ae541.chunk.js"
+  },
+  {
+    "revision": "709cf4b9ab69d8bd6f76",
+    "url": "/static/css/main.9a9af790.chunk.css"
+  },
+  {
+    "revision": "28da4de4fadaa49d209ff68474ec0df0",
+    "url": "/index.html"
+  }
+];
